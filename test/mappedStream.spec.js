644632df1d676b61d9c6bee0838a396ceff45ffe
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-11-14 08:56:11 +0200

    Enhanced MappedStream

--- test/mappedStream.spec.js
+++ test/mappedStream.spec.js
@@ -4,7 +4,10 @@ require('chai')
   .should()
 const { PassThrough } = require('stream')
 const Promise = require('bluebird')
+const logger = require('winston')
 const { MappedStream } = require('../lib')
+
+logger.clear()
 
 describe('MappedStream', () => {
   it('passes through objects', (done) => {
@@ -71,6 +74,50 @@ describe('MappedStream', () => {
         .on('error', reject)
         .on('data', resolve))
       .should.eventually.be.deep.equal(object)
+      .notify(done)
+
+    stream
+      .pipe(mappedStream)
+    stream.write(object)
+    stream.end()
+  })
+
+  it('handles exceptions', (done) => {
+    const object = {
+      property: 'value'
+    }
+
+    const stream = new PassThrough({ objectMode: true })
+    const mappedStream = new MappedStream(() => {
+      throw new Error('expected')
+    })
+    new Promise((resolve, reject) => mappedStream
+        .on('end', resolve)
+        .on('error', reject)
+        .on('data', resolve))
+      .should.eventually.be.rejectedWith(/expected/)
+      .notify(done)
+
+    stream
+      .pipe(mappedStream)
+    stream.write(object)
+    stream.end()
+  })
+
+  it('handles async exceptions', (done) => {
+    const object = {
+      property: 'value'
+    }
+
+    const stream = new PassThrough({ objectMode: true })
+    const mappedStream = new MappedStream(async () => {
+      throw new Error('expected')
+    })
+    new Promise((resolve, reject) => mappedStream
+        .on('end', resolve)
+        .on('error', reject)
+        .on('data', resolve))
+      .should.eventually.be.rejectedWith(/expected/)
       .notify(done)
 
     stream
