162a5eb617b2f9d9079623432a333dca15a7dd93
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-11-14 08:50:45 +0200

    Enhanced ReadableSequence

--- test/readableSequence.spec.js
+++ test/readableSequence.spec.js
@@ -4,7 +4,10 @@ require('chai')
   .should()
 const { PassThrough } = require('stream')
 const Promise = require('bluebird')
+const logger = require('winston')
 const { ReadableSequence } = require('../lib')
+
+logger.clear()
 
 describe('ReadableSequence', () => {
   it('supports array of streams', (done) => {
@@ -94,5 +97,81 @@ describe('ReadableSequence', () => {
         .on('data', (chunk) => { data += chunk }))
       .should.eventually.be.rejectedWith(/expected/)
       .notify(done)
+  })
+
+  it('supports destroy', (done) => {
+    const stream1 = new PassThrough()
+    const stream2 = new PassThrough()
+    const sequence = new ReadableSequence([stream1, stream2])
+    let data = ''
+    new Promise((resolve, reject) => sequence
+        .on('end', () => resolve(data))
+        .on('error', reject)
+        .on('data', (chunk) => { data += chunk }))
+      .should.eventually.be.rejectedWith(/expected/)
+      .notify(done)
+
+    stream1.write('1')
+    sequence.destroy('expected')
+    stream1.write('2')
+  })
+
+  it('handles underlying destroy', (done) => {
+    const stream1 = new PassThrough()
+    const stream2 = new PassThrough()
+    const sequence = new ReadableSequence([stream1, stream2])
+    let data = ''
+    new Promise((resolve, reject) => sequence
+        .on('end', () => resolve(data))
+        .on('error', reject)
+        .on('data', (chunk) => { data += chunk }))
+      .should.eventually.be.equal('12')
+      .notify(done)
+
+    sequence.once('data', () => {
+      stream1.destroy()
+    })
+    stream1.write('1')
+    stream2.write('2')
+    stream2.end()
+  })
+
+  it('supports underlying error', (done) => {
+    const stream = new PassThrough()
+    const sequence = new ReadableSequence([stream])
+    new Promise((resolve, reject) => sequence
+        .on('end', () => {
+          reject(new Error())
+        })
+        .on('error', (error) => {
+          resolve(error)
+        }))
+      .should.eventually.be.equal('expected')
+      .notify(done)
+
+    sequence.once('data', (chunk) => {
+      stream.emit('error', 'expected')
+    })
+    stream.write('1')
+  })
+
+  it('supports underlying error (tolerant)', (done) => {
+    const stream1 = new PassThrough()
+    const stream2 = new PassThrough()
+    const sequence = new ReadableSequence([stream1, stream2], false, false)
+    let data = ''
+    new Promise((resolve, reject) => sequence
+        .on('end', () => resolve(data))
+        .on('error', reject)
+        .on('data', (chunk) => { data += chunk }))
+      .should.eventually.be.equal('12')
+      .notify(done)
+
+    sequence.once('data', () => {
+      stream1.emit('error', 'expected')
+    })
+    stream1.write('1')
+    stream2.write('2')
+    stream2.end()
   })
 })
